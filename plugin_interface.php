01cfc375f33e4b30672bd1f2e7dea51f0305639f
dnesscarkey <dnesscarkey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-21 14:10:52 +0000

    git-svn-id: https://plugins.svn.wordpress.org/any-mobile-theme-switcher/tags/0.2@521808 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin_interface.php
@@ -0,0 +1,193 @@
+<?php
+// create custom plugin settings menu
+add_action('admin_menu', 'any_mobile_create_menu');
+
+function any_mobile_create_menu() {
+	add_options_page('Any Mobile Theme', 'Any Mobile Theme', 'administrator', __FILE__, 'am_settings_page');
+	add_action('admin_init', 'register_mysettings_theme');
+	//add_options_page('Mobile Theme', 'Mobile Theme', 'administrator', __FILE__, 'am_settings_page');
+	//add_action('admin_init', 'register_mysettings_theme');
+}
+
+
+function register_mysettings_theme() {
+	register_setting('am-settings-group', 'iphone_theme');
+	register_setting('am-settings-group', 'ipad_theme');
+	register_setting('am-settings-group', 'android_theme');
+	register_setting('am-settings-group', 'blackberry_theme');
+	register_setting('am-settings-group', 'windows_theme');
+	register_setting('am-settings-group', 'opera_theme');
+	register_setting('am-settings-group', 'palm_theme');
+	register_setting('am-settings-group', 'other_theme');
+}
+
+function am_settings_page() {
+	
+	$iphoneTheme 		= get_option('iphone_theme');
+	$ipadTheme			= get_option('ipad_theme');
+	$androidTheme		= get_option('android_theme');
+	$blackberryTheme	= get_option('blackberry_theme');
+	$windowsTheme		= get_option('windows_theme');
+	$operaTheme			= get_option('opera_theme');
+	$palmTheme			= get_option('palm_theme');
+	$otherTheme			= get_option('other_theme');
+		
+	$themeList 		= get_themes();
+	$themeNames 	= array_keys($themeList); 
+	$defaultTheme 	= get_current_theme();
+	natcasesort($themeNames);
+?>
+<div class="wrap">
+<h2>Any Mobile Theme Switcher</h2>
+<p><em><b>This Plugin is FREE and always will be. Support this plugin by <a href="http://dineshkarki.com.np/donate" target="_blank">donating</a>. Any Amount is Appreciated.</b></em></p>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'am-settings-group' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">iPhone/iPod Touch Theme:</th>
+        <td>
+        	<select name="iphone_theme"  />
+     <?php 
+      foreach ($themeNames as $themeName) {              
+          if (($iphoneTheme == $themeName) || (($iphoneTheme == '') && ($themeName == $defaultTheme))) {
+              echo '<option value="' . $themeName . '" selected="selected">' . htmlspecialchars($themeName) . '</option>';
+          } else {
+              echo '<option value="' . $themeName . '">' . htmlspecialchars($themeName) . '</option>';
+          }
+      }
+     ?>
+        	</select>
+        </td>
+        </tr>
+         
+        <tr valign="top">
+        <th scope="row">iPad Theme</th>
+        <td>
+        	<select name="ipad_theme"  />
+     <?php 
+      foreach ($themeNames as $themeName) {              
+          if (($ipadTheme == $themeName) || (($ipadTheme == '') && ($themeName == $defaultTheme))) {
+              echo '<option value="' . $themeName . '" selected="selected">' . htmlspecialchars($themeName) . '</option>';
+          } else {
+              echo'<option value="' . $themeName . '">' . htmlspecialchars($themeName) . '</option>';
+          }
+      }
+     ?>
+        	</select>
+        </td>
+        </tr>
+        
+        <tr valign="top">
+        <th scope="row">Android Theme</th>
+        <td>
+        	<select name="android_theme"  />
+     <?php 
+      foreach ($themeNames as $themeName) {              
+          if (($androidTheme == $themeName) || (($androidTheme == '') && ($themeName == $defaultTheme))) {
+              echo '<option value="' . $themeName . '" selected="selected">' . htmlspecialchars($themeName) . '</option>';
+          } else {
+              echo'<option value="' . $themeName . '">' . htmlspecialchars($themeName) . '</option>';
+          }
+      }
+     ?>
+        	</select>
+        </td>
+        </tr>
+		
+		
+		<tr valign="top">
+        <th scope="row">Blackberry Theme</th>
+        <td>
+        	<select name="blackberry_theme"  />
+     <?php 
+      foreach ($themeNames as $themeName) {              
+          if (($blackberryTheme == $themeName) || (($blackberryTheme == '') && ($themeName == $defaultTheme))) {
+              echo '<option value="' . $themeName . '" selected="selected">' . htmlspecialchars($themeName) . '</option>';
+          } else {
+              echo'<option value="' . $themeName . '">' . htmlspecialchars($themeName) . '</option>';
+          }
+      }
+     ?>
+        	</select>
+        </td>
+        </tr>
+        
+        
+        <tr valign="top">
+        <th scope="row">Windows Mobile Theme</th>
+        <td>
+        	<select name="windows_theme"  />
+			 <?php 
+              foreach ($themeNames as $themeName) {              
+                  if (($windowsTheme == $themeName) || (($windowsTheme == '') && ($themeName == $defaultTheme))) {
+                      echo '<option value="' . $themeName . '" selected="selected">' . htmlspecialchars($themeName) . '</option>';
+                  } else {
+                      echo'<option value="' . $themeName . '">' . htmlspecialchars($themeName) . '</option>';
+                  }
+              }
+             ?>
+        	</select>
+        </td>
+        </tr>
+        
+        <tr valign="top">
+        <th scope="row">Opera Mini Theme</th>
+        <td>
+        	<select name="opera_theme"  />
+			 <?php 
+              foreach ($themeNames as $themeName) {              
+                  if (($operaTheme == $themeName) || (($operaTheme == '') && ($themeName == $defaultTheme))) {
+                      echo '<option value="' . $themeName . '" selected="selected">' . htmlspecialchars($themeName) . '</option>';
+                  } else {
+                      echo'<option value="' . $themeName . '">' . htmlspecialchars($themeName) . '</option>';
+                  }
+              }
+             ?>
+        	</select>
+        </td>
+        </tr>
+        
+        <tr valign="top">
+        <th scope="row">Parm Os Theme</th>
+        <td>
+        	<select name="parm_theme"  />
+			 <?php 
+              foreach ($themeNames as $themeName) {              
+                  if (($palmTheme == $themeName) || (($palmTheme == '') && ($themeName == $defaultTheme))) {
+                      echo '<option value="' . $themeName . '" selected="selected">' . htmlspecialchars($themeName) . '</option>';
+                  } else {
+                      echo'<option value="' . $themeName . '">' . htmlspecialchars($themeName) . '</option>';
+                  }
+              }
+             ?>
+        	</select>
+        </td>
+        </tr>
+        
+        <tr valign="top">
+        <th scope="row">Other Mobile Device Theme</th>
+        <td>
+        	<select name="other_theme"  />
+			 <?php 
+              foreach ($themeNames as $themeName) {              
+                  if (($otherTheme == $themeName) || (($otherTheme == '') && ($themeName == $defaultTheme))) {
+                      echo '<option value="' . $themeName . '" selected="selected">' . htmlspecialchars($themeName) . '</option>';
+                  } else {
+                      echo'<option value="' . $themeName . '">' . htmlspecialchars($themeName) . '</option>';
+                  }
+              }
+             ?>
+        	</select>
+        </td>
+        </tr>
+		
+    </table>
+    
+    <p class="submit">
+    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+    </p>
+
+</form>
+</div>
+<?php } ?>
